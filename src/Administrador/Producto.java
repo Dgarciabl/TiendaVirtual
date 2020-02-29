7223193183bcbf93df1f32919ec294a1351d1eab
VinNeko-ZX <55595309+VinNeko-ZX@users.noreply.github.com>
2020-02-29 14:30:24 -0500

    Producto (IMCOMPLETO)

--- src/Administrador/Producto.java
+++ src/Administrador/Producto.java
@@ -1,5 +1,33 @@
 package Administrador;
+import java.util.*;
 
 public class Producto {
+	
+	//Atributos
+	private String nombre;
+	private String descripcion;
+	private ArrayList<Categoria> categorias;
+	private int precioVenta;
+	private int precioCompra;
+	private float calificacionProducto;
+	
+	//Constructores
+	Producto(String n,String d, ArrayList<Categoria> cat, int pv, int pc, float cp){
+		this.nombre=n; this.descripcion=d; this.categorias=cat;
+		this.precioVenta=pv; this.precioCompra=pc; this.calificacionProducto=cp;
+	}
+	
+	//Getters
+	public String GetNombre() {return this.nombre;}
+	
+	public String GetDescripcion() {return this.descripcion;}
+	
+	public ArrayList<Categoria> GetCategoria() {return this.categorias;}
+	
+	public int GetPrecioVenta() {return this.precioVenta;}
+	
+	public int GetPrecioCompra() {return this.precioCompra;}
+	
+	
 
 }

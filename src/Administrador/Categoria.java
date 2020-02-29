b6004f7be126c035da09c3dc3f5fafbe69050099
VinNeko-ZX <55595309+VinNeko-ZX@users.noreply.github.com>
2020-02-29 13:37:50 -0500

    Categoria

--- src/Administrador/Categoria.java
+++ src/Administrador/Categoria.java
@@ -1,5 +1,24 @@
 package Administrador;
 
 public class Categoria {
+	private String nombre;
+	private String descripcion;
+	
+	
+	public String GetDescripcion() {
+		return this.descripcion;
+	}
+	
+	void SetDescripcion(String d) {
+		this.descripcion=d;
+	}
+	
+	public String GetNombre() {
+		return this.nombre;
+	}
+	
+	void SetNombre(String d) {
+		this.nombre=d;
+	}
 
 }

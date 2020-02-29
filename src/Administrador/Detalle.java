3511b9fcb41db87e8f28af76a9626f456c487fe7
VinNeko-ZX <55595309+VinNeko-ZX@users.noreply.github.com>
2020-02-29 14:17:30 -0500

    Detalle V2

--- src/Administrador/Detalle.java
+++ src/Administrador/Detalle.java
@@ -1,19 +1,29 @@
 package Administrador;
 
 public class Detalle {
+	
+	//Atributos
 	private Producto producto;
 	private int cantidad;
 	
+	//Constructores
+	Detalle(Producto p, int c){
+		this.producto=p;
+		this.cantidad=c;
+	}
+	
+	//Getters
 	public Producto GetProducto() {
 		return this.producto;
 	}
 	
-	void SetProducto(Producto k) {
-		this.producto=k;
-	}
-	
 	public int GetCantidad() {
 		return this.cantidad;
+	}
+	
+	//Setters
+	void SetProducto(Producto k) {
+		this.producto=k;
 	}
 	
 	void SetCantidad(int i) {

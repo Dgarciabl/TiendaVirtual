b4f151c55ff795c68e572898ca7e7e43e5b4a506
Dgarciabl <Dgarciabl@users.noreply.github.com>
2020-03-01 12:47:00 -0500

    basic main

--- src/UImain/Main.java
+++ src/UImain/Main.java
@@ -1,7 +1,26 @@
 package UImain;
-
+import java.util.*;
+import gestorAplicacion.Usuario.*;
+import gestorAplicacion.Administrador.*;
+import UImain.*;
+import UImain.Opciones.*;
 public class Main {
+	public static ArrayList<Persona> Usuarios;
+	public static Inventario inventario;
+	public static ArrayList<Categoria> categorias;
+	public static ArrayList<Producto> productos;
+	public static Estadistica estadisticos;
+	public static int nivel=2;
+	public static MenuDeConsola[] menu= new MenuDeConsola[3];
 	public static void main(String[] args){
 		
+	}
+	public void montarDB() {
+		//Usuarios;
+		//inventario;
+		//categorias;
+		//productos;
+		//estadisticos;
+		//menu;
 	}
 }

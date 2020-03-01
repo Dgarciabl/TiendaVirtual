db3c4e49b0cf5871bece1f6e7260986d2fc40f72
Dgarciabl <Dgarciabl@users.noreply.github.com>
2020-03-01 16:50:49 -0500

    buscar producto opcion

---
+++ src/UImain/Opciones/OpcionBuscarProducto.java
@@ -0,0 +1,13 @@
+package UImain.Opciones;
+
+import UImain.OpcionDeMenu;
+
+public class OpcionBuscarProducto implements OpcionDeMenu {
+
+	@Override
+	public void ejecutar() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

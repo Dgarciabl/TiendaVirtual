30b76afb9f456217baf6f64a4389d9ee7b3bf9ad
Dgarciabl <Dgarciabl@users.noreply.github.com>
2020-03-01 15:52:32 -0500

    cerrar sesion

---
+++ src/UImain/Opciones/OpcionCerrarSesion.java
@@ -0,0 +1,17 @@
+package UImain.Opciones;
+
+import UImain.Main;
+import UImain.OpcionDeMenu;
+
+public class OpcionCerrarSesion implements OpcionDeMenu {
+	@Override
+	public void ejecutar() {
+		Main.usuario=null;
+		Main.nivel=2;
+	}
+	@Override
+	public String toString() {
+		return "Cerrar Sesion";
+	}
+
+}

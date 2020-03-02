0571cca3aad207d1bd2121da0a6aee00128ae641
JulianFM1 <JulianFM1@users.noreply.github.com>
2020-03-02 18:06:26 -0500

    nn

--- src/UImain/Opciones/Admin/OpcionModificarDescripcionCategoria.java
+++ src/UImain/Opciones/Admin/OpcionModificarDescripcionCategoria.java
@@ -5,9 +5,8 @@ import UImain.Main;
 import UImain.OpcionModificar;
 import gestorAplicacion.Administrador.Administrador;
 
-public class OpcionModificarNombreCategoria implements OpcionModificar {
+public class OpcionModificarDescripcionCategoria implements OpcionModificar {
 	Scanner input = new Scanner(System.in);
-	
 	@Override
 	public void ejecutar() {
 		
@@ -17,13 +16,14 @@ public class OpcionModificarNombreCategoria implements OpcionModificar {
 		}
 		System.out.println("Que categoria desea modificar?");
 		int x = input.nextInt();
-		System.out.println("Por cual nombre desea remplazarlo?");
+		System.out.println("Descripcion:/n");
+		System.out.println(Main.categorias.get(x).getDescripcion());
+		System.out.println("/nPor cual desea remplazarla?");
 		String str1 = input.next();
-		admon.modificarNombreCategoria(x, str1);
+		admon.modificarDescripcionCategoria(x, str1);
 	}
 	public String toString() {
-		return "MODIFICAR NOMBRE CATEGORIA";
+		return "MODIFICAR DESCRIPCION PRODUCTO";
 	}
-
 
 }

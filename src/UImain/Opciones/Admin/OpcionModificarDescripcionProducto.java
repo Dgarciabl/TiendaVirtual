01891ba248834cc4c4ab237f77205a9d5b42ab5d
Dgarciabl <Dgarciabl@users.noreply.github.com>
2020-03-01 19:52:07 -0500

    opcion modificar

---
+++ src/UImain/Opciones/Admin/OpcionModificarDescripcionProducto.java
@@ -0,0 +1,13 @@
+package UImain.Opciones.Admin;
+
+import UImain.OpcionModificar;
+
+public class OpcionModificarDescripcionProducto implements OpcionModificar {
+
+	@Override
+	public void ejecutar() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/UImain/Opciones/Admin/OpcionesModificarPrecioVentaProducto.java
@@ -0,0 +1,13 @@
+package UImain.Opciones.Admin;
+
+import UImain.OpcionModificar;
+
+public class OpcionesModificarPrecioVentaProducto implements OpcionModificar {
+
+	@Override
+	public void ejecutar() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

b64d872221f770dc4d3e8523b7cdb1bf72bcb91c
JulianFM1 <JulianFM1@users.noreply.github.com>
2020-03-01 20:53:32 -0500

    Opcion CrearExistencias Final

--- src/UImain/Opciones/Admin/OpcionCrearExistencias.java
+++ src/UImain/Opciones/Admin/OpcionCrearExistencias.java
@@ -2,27 +2,34 @@ package UImain.Opciones.Admin;
 import UImain.OpcionCreacion;
 import UImain.Main;
 import gestorAplicacion.Administrador.Producto;
-import gestorAplicacion.Administrador.inventario;
+import gestorAplicacion.Administrador.Inventario;
+import gestorAplicacion.Administrador.Detalle;
 import java.util.*;
 
 public class OpcionCrearExistencias implements OpcionCreacion {
 
 	public void ejecutar(){
-//		busca por cada producto y busca en el inventario
-//		buscar los productos los que estan en productos pero no en inventario,
+//		buscar por cada producto en el inventario
+//		los que estan en productos pero no en inventario,
 //		y el admin escoge por indice y yo pregunto la cantidad y con esa cantidad 
 //		modifico el un detalle con ese producto y esa cantidad.
+		
 		Scanner input = new Scanner(System.in);
+		int i=0;
 		
 		for(Producto prod : Main.productos){
-			String str1 = prod.getNombre();
-			for(De)
-				
-				//crear opocion modificar nombre , descripcion crear clase que herede de opcionmodificar
 			
-			
+			for(Detalle deta : Main.inventario.getInventario()){	
+				i+=1;
+				if(deta.getProducto().getNombre() == prod.getNombre()){
+					System.out.println(prod.getNombre()+"  "+i);
+				}
+			}			
 		}
+		System.out.println("Escoja el indice del producto que desea añadir:");
+		int x = input.nextInt();
+		System.out.println("Que cantidad desea añadir");
+		int y = input.nextInt();
 		
 	}
-
 }

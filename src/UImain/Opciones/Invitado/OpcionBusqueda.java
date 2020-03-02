6a0184bab9c500e32d08ace4eb51437b7a518877
VinNeko-ZX <55595309+VinNeko-ZX@users.noreply.github.com>
2020-03-01 19:17:28 -0500

    Opciones Actualizadas
    Divididas las Opciones de Busqueda en sus archivos correspondientes

--- src/UImain/Opciones/Invitado/OpcionBusqueda.java
+++ src/UImain/Opciones/Invitado/OpcionBusqueda.java
@@ -1,12 +1,31 @@
 package UImain.Opciones.Invitado;
 
+import java.util.*;
+
 import UImain.OpcionDeMenu;
+import uiMain.Main;
+import gestorAplicacion.Administrador.Producto;
 
 public class OpcionBusqueda implements OpcionDeMenu {
 
 	@Override
 	public void ejecutar() {
-		// TODO Auto-generated method stub
+		Scanner Ing=new Scanner(System.in);
+		System.out.println("Escriba el nombre de la categoria");
+		String l=Ing.next();
+		int s=Main.categorias.indexOf(l);
+		ArrayList<Producto> Reg=Main.inventario.RealizarBusqueda(s);
+		if (Reg.size()!=0) {
+			System.out.println("Se han encontrado los siguientes productos");
+			for (int m=0;m<Reg.size();m++) {
+				Producto h=Reg.get(m);
+				System.out.println("Nombre: "+h.getNombre()); System.out.println("Descripcion: "+h.getDescripcion());
+				System.out.println("Precio: "+h.getPrecioVenta()); System.out.println("--------------");
+			}
+		}
+		else {
+			System.out.println("No se han encontrado porductos");
+		}
 
 	}
 

550118e49b590530d8752ecffcce5daee6799c7e
JulianFM1 <JulianFM1@users.noreply.github.com>
2020-03-02 15:17:44 -0500

    Final AddExistenciatoInventario

--- src/UImain/Opciones/Admin/OpcionCrearExistencias.java
+++ src/UImain/Opciones/Admin/OpcionCrearExistencias.java
@@ -9,27 +9,31 @@ import java.util.*;
 public class OpcionCrearExistencias implements OpcionCreacion {
 
 	public void ejecutar(){
-//		buscar por cada producto en el inventario
-//		los que estan en productos pero no en inventario,
-//		y el admin escoge por indice y yo pregunto la cantidad y con esa cantidad 
-//		modifico el un detalle con ese producto y esa cantidad.
 		
 		Scanner input = new Scanner(System.in);
-		int i=0;
+		ArrayList<Producto> mostrar = new ArrayList<>();
+		ArrayList<Integer> Indices = new ArrayList<>();
+		int i = 0;
 		
 		for(Producto prod : Main.productos){
-			
+			i+=1;
 			for(Detalle deta : Main.inventario.getInventario()){	
-				i+=1;
-				if(deta.getProducto().getNombre() == prod.getNombre()){
-					System.out.println(prod.getNombre()+"  "+i);
+				if(!((deta.getProducto().getNombre()).equals(prod.getNombre()))){
+					mostrar.add(prod);
+					Indices.add(i);
 				}
-			}			
+			}
 		}
+		
+		for(int j=0; j<mostrar.size(); j+=1){
+			System.out.println(Indices.get(j)+". "+mostrar.get(j).getNombre());
+		}
+		
 		System.out.println("Escoja el indice del producto que desea añadir:");
 		int x = input.nextInt();
 		System.out.println("Que cantidad desea añadir");
 		int y = input.nextInt();
-		
+		Detalle detail = new Detalle(Main.productos.get(Indices.get(x)), y);
+	
 	}
 }

2eff8d86d06324991d8c4cd747e5425a3d34c187
Dgarciabl <Dgarciabl@users.noreply.github.com>
2020-03-01 19:00:33 -0500

    crear existencias

---
+++ src/UImain/Opciones/Admin/OpcionCrearExistencias.java
@@ -0,0 +1,13 @@
+package UImain.Opciones.Admin;
+
+import UImain.OpcionCreacion;
+
+public class OpcionCrearExistencias implements OpcionCreacion {
+
+	@Override
+	public void ejecutar() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

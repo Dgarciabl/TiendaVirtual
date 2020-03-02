fa76c6f096643185a10098f0beccc86c63c5e784
JulianFM1 <JulianFM1@users.noreply.github.com>
2020-03-01 20:03:23 -0500

    CrearEcistencias

--- src/UImain/Opciones/Admin/OpcionCrearExistencias.java
+++ src/UImain/Opciones/Admin/OpcionCrearExistencias.java
@@ -1,13 +1,28 @@
 package UImain.Opciones.Admin;
-
 import UImain.OpcionCreacion;
+import UImain.Main;
+import gestorAplicacion.Administrador.Producto;
+import gestorAplicacion.Administrador.inventario;
+import java.util.*;
 
 public class OpcionCrearExistencias implements OpcionCreacion {
 
-	@Override
-	public void ejecutar() {
-		// TODO Auto-generated method stub
-
+	public void ejecutar(){
+//		busca por cada producto y busca en el inventario
+//		buscar los productos los que estan en productos pero no en inventario,
+//		y el admin escoge por indice y yo pregunto la cantidad y con esa cantidad 
+//		modifico el un detalle con ese producto y esa cantidad.
+		Scanner input = new Scanner(System.in);
+		
+		for(Producto prod : Main.productos){
+			String str1 = prod.getNombre();
+			for(De)
+				
+				//crear opocion modificar nombre , descripcion crear clase que herede de opcionmodificar
+			
+			
+		}
+		
 	}
 
 }

fffcf51d1265097cee2f239af6e9f00d67dc4d4d
JulianFM1 <JulianFM1@users.noreply.github.com>
2020-03-03 14:41:49 -0500

    Update OpcionEliminarProductoCarro.java
    eso

--- src/UImain/Opciones/Usuario/OpcionEliminarProductoCarro.java
+++ src/UImain/Opciones/Usuario/OpcionEliminarProductoCarro.java
@@ -1,13 +1,37 @@
 package UImain.Opciones.Usuario;
 
+import java.util.*;
+import UImain.Main;
 import UImain.OpcionDeMenu;
+import gestorAplicacion.Usuario.Usuario;
 
 public class OpcionEliminarProductoCarro implements OpcionDeMenu {
-
+	Scanner input = new Scanner(System.in);
 	@Override
 	public void ejecutar() {
-		// TODO Auto-generated method stub
+		if(Main.usuario instanceof Usuario) {
+			Usuario us1 = (Usuario)Main.usuario;
+			for(int i=0; i<us1.getCarro().getInventario().size(); i++) {
+				System.out.println(i + ". "+us1.getCarro().getInventario().get(i));
+			}
+			System.out.println("Que producto desea eliminar del carro?");
+			int x = input.nextInt();
+			System.out.println("Esta seguro que desea eliminar "+us1.getCarro().getInventario().get(x)+" del carro? S/N");
+			String str1 = input.next();
+			if(str1.equals("S")) {
+				us1.eliminarProducto(x);
+			}else {
+				System.out.println("Accion cancelada");
+			}
+		}else {
+			System.out.println("No valido para usuario");
+		}
 
+	}
+	
+	@Override
+	public String toString() {
+		return "Eliminar Produco Carro";
 	}
 
 }

a52dbc52f19f8df3e9252337e86360ff8d47c93e
Dgarciabl <Dgarciabl@users.noreply.github.com>
2020-03-04 09:37:28 -0500

    Create OpcionAñadirSaldo.java

---
+++ src/UImain/Opciones/Usuario/OpcionAñadirSaldo.java
@@ -0,0 +1,29 @@
+package UImain.Opciones.Usuario;
+
+import UImain.Main;
+import UImain.OpcionDeMenu;
+import gestorAplicacion.Usuario.*;
+import java.util.*;
+public class OpcionAñadirSaldo implements OpcionDeMenu {
+
+	@Override
+	public void ejecutar() {
+		Scanner in = new Scanner(System.in);
+		if(Main.usuario instanceof Usuario) {
+			Usuario user=(Usuario)Main.usuario;
+			System.out.println("Que cantidad desea añadir");
+			double sal=in.nextDouble();
+			if(sal>0) {
+				user.actualizarSaldo(sal);
+			}else {
+				System.out.println("no es un monto valido");
+			}
+		}else {
+			System.out.println("No es usuario valido");
+		}
+	}
+	@Override
+	public String toString() {
+		return "Añadir saldo";
+	}
+}

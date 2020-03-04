7ff7aed2f1051c4e1b0fef0788837b6b0b8e72ff
VinNeko-ZX <55595309+VinNeko-ZX@users.noreply.github.com>
2020-03-03 23:02:22 -0500

    Validacion 3

--- src/UImain/Opciones/Admin/OpcionCrearUsuario.java
+++ src/UImain/Opciones/Admin/OpcionCrearUsuario.java
@@ -1,7 +1,7 @@
 package UImain.Opciones.Admin;
 
 import UImain.OpcionDeMenu;
-import java.util.Scanner;
+import java.util.*;
 import UImain.Main;
 import gestorAplicacion.Administrador.*;
 public class OpcionCrearUsuario implements OpcionDeMenu {
@@ -13,25 +13,58 @@ public class OpcionCrearUsuario implements OpcionDeMenu {
 			Administrador admon=(Administrador)Main.usuario;
 			System.out.println("Nombre del cliente");
 			String nombre=in.next();
+			if (Main.isNumeric(nombre)) {
+				System.out.println("El nombre no puede ser un numero");
+			}
+			else {
 			System.out.println("Genero f/m");
 			String input=in.next();
-			boolean genero;
-			if (input.equals("f")) {
-				genero=false;
-			}else {
-				genero=true;
+			if (input.equals("f") || input.equals("m")) {							
+				boolean genero;
+				if (input.equals("f")) {
+					genero=false;
+				}else {
+					genero=true;
+				}
+				System.out.println("edad");
+				try {
+				int edad=in.nextInt();
+				System.out.println("Nombre de usuario deseado");
+				String usuario=in.next();
+				if (Main.isNumeric(usuario)) {
+					System.out.println("El usuario no puede ser un numero");
+				}
+				else {
+				System.out.println("Digite la contraseña");
+				String contraseña=in.next();
+				System.out.println("Escriba una pregunta de seguridad");
+				String pregunta=in.next();
+				if (Main.isNumeric(pregunta)) {
+					System.out.println("La pregunta no puede ser un numero");
+				}
+				else {
+					System.out.println("digite la respuesta a la pregunta anterior");
+					String respuesta=in.next();
+					if (Main.isNumeric(respuesta)) {
+						System.out.println("La respuesta no puede ser un numero");
+					}
+					else {			
+						admon.CrearUsuario(nombre,genero,edad,usuario,contraseña,pregunta,respuesta);
+						}
+						}
+					}
+				}
+				
+				catch(InputMismatchException e) {
+					System.out.println("Error en el input, intente nuevamente");
+				}
 			}
-			System.out.println("edad");
-			int edad=in.nextInt();
-			System.out.println("Nombre de usuario deseado");
-			String usuario=in.next();
-			System.out.println("Digite la contraseña");
-			String contraseña=in.next();
-			System.out.println("Escriba una pregunta de seguridad");
-			String pregunta=in.next();
-			System.out.println("digite la respuesta a la pregunta anterior");
-			String respuesta=in.next();
-			admon.CrearUsuario(nombre,genero,edad,usuario,contraseña,pregunta,respuesta);
+			
+			else {
+				System.out.println("El genero es incorrecto");
+			
+			}
+			}
 		}
 		//in.close();
 	}
--- src/UImain/Opciones/Usuario/OpcionCambiarContraseña.java
+++ src/UImain/Opciones/Usuario/OpcionCambiarContraseña.java
@@ -19,6 +19,9 @@ public class OpcionCambiarContraseña implements OpcionDeMenu {
 		}
 		
 	}
+	public String toString() {
+		return "Cambiar Contraseña";
+	}
 	
 
 }

6610faa506783edd7f906e7a3eb72f312f761fb6
Dgarciabl <Dgarciabl@users.noreply.github.com>
2020-03-04 00:46:22 -0500

    opcion

--- src/UImain/Opciones/Admin/OpcionAñadirOpcionAdministrador.java
+++ src/UImain/Opciones/Admin/OpcionAñadirOpcionAdministrador.java
@@ -1,5 +1,6 @@
 package UImain.Opciones.Admin;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import UImain.Main;
@@ -12,10 +13,25 @@ public class OpcionAñadirOpcionAdministrador implements OpcionDeMenu {
 	public void ejecutar() {
 		if(Main.usuario instanceof Administrador) {
 			Scanner in = new Scanner(System.in);
-			Main.full.lanzarMenu();
+			boolean print;
+			ArrayList<Integer> dic=new ArrayList<Integer>();
+			int k=0;
+			for (int i=0;i<Main.full.opciones.size();i++) {
+				print=true;
+				for (int j=0;j<Main.menu[0].opciones.size();j++) {
+					if(Main.full.opciones.get(i).getClass()==Main.menu[0].opciones.get(j).getClass()) {
+						print=false;
+					}
+				}
+				if(print) {
+					dic.add(i);
+					System.out.println(k+") "+Main.full.opciones.get(i));
+					k++;
+				}
+			}
 			System.out.println("Selecione el indice de la opcion que desea añadir");
 			int op=in.nextInt();
-			Main.menu[0].añadirOpcion(Main.full.opciones.get(op));
+			Main.menu[0].añadirOpcion(Main.full.opciones.get(dic.get(op)));
 		}else {
 			System.out.println("El tipo de usuario no es valido");
 		}
--- src/UImain/Opciones/Admin/OpcionAñadirOpcionUsuario.java
+++ src/UImain/Opciones/Admin/OpcionAñadirOpcionUsuario.java
@@ -5,17 +5,32 @@ import java.util.Scanner;
 import UImain.Main;
 import UImain.OpcionDeMenu;
 import gestorAplicacion.Administrador.Administrador;
-
+import java.util.ArrayList;
 public class OpcionAñadirOpcionUsuario implements OpcionDeMenu {
 
 	@Override
 	public void ejecutar() {
 		if(Main.usuario instanceof Administrador) {
 			Scanner in = new Scanner(System.in);
-			Main.full.lanzarMenu();
+			boolean print;
+			ArrayList<Integer> dic=new ArrayList<Integer>();
+			int k=0;
+			for (int i=0;i<Main.full.opciones.size();i++) {
+				print=true;
+				for (int j=0;j<Main.menu[1].opciones.size();j++) {
+					if(Main.full.opciones.get(i).getClass()==Main.menu[1].opciones.get(j).getClass()) {
+						print=false;
+					}
+				}
+				if(print) {
+					dic.add(i);
+					System.out.println(k+") "+Main.full.opciones.get(i));
+					k++;
+				}
+			}
 			System.out.println("Selecione el indice de la opcion que desea añadir");
 			int op=in.nextInt();
-			Main.menu[1].añadirOpcion(Main.full.opciones.get(op));
+			Main.menu[1].añadirOpcion(Main.full.opciones.get(dic.get(op)));
 		}else {
 			System.out.println("El tipo de usuario no es valido");
 		}

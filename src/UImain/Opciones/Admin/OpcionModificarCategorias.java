c8518930e16b70adbb478f5ee29501fe7b2356af
JulianFM1 <JulianFM1@users.noreply.github.com>
2020-03-03 21:06:47 -0500

    Update OpcionModificarCategorias.java
    eso

--- src/UImain/Opciones/Admin/OpcionModificarCategorias.java
+++ src/UImain/Opciones/Admin/OpcionModificarCategorias.java
@@ -19,35 +19,34 @@ public class OpcionModificarCategorias implements OpcionDeMenu {
 			}
 			System.out.println("Que producto desea modificar?");
 			int x = input.nextInt();
-			System.out.println("Categorias:/n");
+			System.out.println("Categorias:");
 			
 			if(Main.productos.get(x).getCategoria().size()>0) {
 				for(int i=0;i<(Main.productos.get(x).getCategoria()).size(); i++) {
 					System.out.println(i + ". "+(Main.productos.get(x).getCategoriaIndividual(i).getNombre()));
+					
+					System.out.println("Que categoria del producto desea eliminar?");
+					int y = input.nextInt();
+					admon.delCategoriaProducto(x, Main.productos.get(x).getCategoriaIndividual(y));
+					
+					System.out.println("Desea agregar una nueva categoria al producto? S/N");
+					String str1 = input.next();
+					if(str1.equals("S")) {
+						System.out.println("Que categoria desea añadir?");
+						for(int j=0;j<(Main.categorias.size());j++) {
+							System.out.println(j + ". "+Main.categorias.get(j).getNombre());
+						}
+						int z = input.nextInt();
+						admon.addCategoriaProducto(x, Main.categorias.get(z));
+					}
 				}
 			}else {
 				System.out.println("El producto no cuenta con categorias");
 			}
-
-			
-			System.out.println("Que categoria del producto desea eliminar?");
-			int y = input.nextInt();
-			admon.delCategoriaProducto(x, Main.productos.get(x).getCategoriaIndividual(y));
-			
-			System.out.println("Desea agregar una nueva categoria al producto? S/N");
-			String str1 = input.next();
-			if(str1.equals("S")) {
-				System.out.println("Que categoria desea añadir?");
-				for(int j=0;j<(Main.categorias.size());j++) {
-					System.out.println(j + ". "+Main.categorias.get(j).getNombre());
-				}
-				int z = input.nextInt();
-				admon.addCategoriaProducto(x, Main.categorias.get(z));
-			}else {
+		}else {
 				System.out.println("El usuario no es valido");
 			}
 		}
-	}
 	
 	@Override
 	public String toString() {

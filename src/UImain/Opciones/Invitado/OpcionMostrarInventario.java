0ace35fb21c970040130b76956fa98211393b2b4
Dgarciabl <Dgarciabl@users.noreply.github.com>
2020-03-04 00:00:36 -0500

    mostrar inventario

--- src/UImain/Opciones/Invitado/OpcionMostrarInventario.java
+++ src/UImain/Opciones/Invitado/OpcionMostrarInventario.java
@@ -2,20 +2,19 @@ package UImain.Opciones.Invitado;
 
 import UImain.Main;
 import UImain.OpcionDeMenu;
-import gestorAplicacion.Administrador.Administrador;
-import gestorAplicacion.Usuario.Usuario;
 
-public class OpcionMostrarProductos implements OpcionDeMenu {
+public class OpcionMostrarInventario implements OpcionDeMenu {
 	@Override
 	public void ejecutar() {
 		if(Main.usuario!=null) {
-			System.out.println(Main.usuario.mostrarProductos());
+			System.out.println(Main.inventario.toString());
 		}else {
 			System.out.println("-------------------------------------\nProductos:\n-------------------------------------\n"+Main.inventario.toString());
 		}
 	}
 	@Override
 	public String toString(){
-		return "Mostrar Productos";
+		return "Mostrar Inventario";
 	}
+
 }

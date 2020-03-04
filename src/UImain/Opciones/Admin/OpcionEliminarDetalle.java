cc53265b9887a5192497a008b668c0ece6624b3a
VinNeko-ZX <55595309+VinNeko-ZX@users.noreply.github.com>
2020-03-03 23:35:32 -0500

    validacion 4

--- src/UImain/Opciones/Admin/OpcionEliminarDetalle.java
+++ src/UImain/Opciones/Admin/OpcionEliminarDetalle.java
@@ -16,15 +16,28 @@ public class OpcionEliminarDetalle implements OpcionDeMenu {
 			for(int i=0; i<Main.inventario.getInventario().size();i++) {
 				System.out.println(i+ ". "+Main.inventario.getInventario().get(i).getProducto().getNombre());
 			}
-			System.out.println("Que roducto desea eliminar?");
+			System.out.println("Que producto desea eliminar?");
+			try {
 			int x = input.nextInt();
 			System.out.println("El producto: "+Main.inventario.getInventario().get(x).getProducto().getNombre()+" Sera completamente eliminado, esta seguro? S/N");
 			String str1 = input.next();
-			input.close();
-			if(str1.equals("S")) {
-				admon.eliminarDetalle(x);
-			}else {
-				System.out.println("Operacion Cancelada");
+			if (str1.equals("S") || str1.equals("N")) {				
+				//input.close();
+				if(str1.equals("S")) {
+					admon.eliminarDetalle(x);
+				}else {
+					System.out.println("Operacion Cancelada");
+				}
+				}
+			else {
+				System.out.println("Error en la confirmacion");
+			}
+			}
+			catch (InputMismatchException e) {
+				System.out.println("Error en input, intentelo nuevamente");
+			}
+			catch (IndexOutOfBoundsException s) {
+				System.out.println("El detalle no existe");
 			}
 		}else {
 			System.out.println("Usuario Invalido");

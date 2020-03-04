d6f979df201b6eeeea84fe90e3e7f0ce0c0717d2
JulianFM1 <JulianFM1@users.noreply.github.com>
2020-03-03 21:17:21 -0500

    Update OpcionModificarCategorias.java
    wao

--- src/UImain/Opciones/Admin/OpcionModificarCategorias.java
+++ src/UImain/Opciones/Admin/OpcionModificarCategorias.java
@@ -43,6 +43,17 @@ public class OpcionModificarCategorias implements OpcionDeMenu {
 				}
 			}else {
 				System.out.println("El producto no cuenta con categorias");
+				System.out.println("Desea agregar una nueva categoria al producto? S/N");
+				String str1 = input.next();
+				if(str1.equals("S")) {
+					System.out.println("Que categoria desea añadir?");
+					for(int j=0;j<(Main.categorias.size());j++) {
+						System.out.println(j + ". "+Main.categorias.get(j).getNombre());
+					}
+					int z = input.nextInt();
+					admon.addCategoriaProducto(x, Main.categorias.get(z));
+				}
+				
 			}
 		}else {
 				System.out.println("El usuario no es valido");

4c9c392e7a275a5f652b3d070f6001bf43a8f993
Dgarciabl <Dgarciabl@users.noreply.github.com>
2020-03-16 19:13:16 -0500

    Create Main.java

---
+++ src/UImain/Main.java
@@ -0,0 +1,96 @@
+package UImain;
+
+import java.util.*;
+import gestorAplicacion.Usuario.*;
+import gestorAplicacion.Administrador.*;
+import UImain.Opciones.Invitado.*;
+import UImain.Opciones.Usuario.*;
+import UImain.Opciones.Admin.*;
+import javafx.application.Application;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+
+public class Main extends Application {
+	public static ArrayList<Persona> Usuarios;
+	public static Inventario inventario;
+	public static ArrayList<Categoria> categorias;
+	public static ArrayList<Producto> productos;
+	public static Estadistica estadisticos;
+	public static int nivel=2;
+	public static Persona usuario;
+	public static void main(String[] args){
+		while(true) {
+			launch();
+		}
+	}
+	public void montarDB() {
+		//Usuarios;
+		//inventario;
+		//categorias;
+		//productos;
+		//estadisticos;
+		//menu;
+	}
+	public static void cargarUsuarios() {
+		Usuarios=new ArrayList<Persona>();
+		Administrador admon=new Administrador("superadmin", true, 0, "root", "root", "root", "root");
+		Usuarios.add(admon);
+		Administrador admon2 = new Administrador("Jorge", true, 54, "jordi", "soloyolase", "NombrePadre", "Javier");
+		Usuarios.add(admon2);
+		Administrador admon3 = new Administrador("Daniela", false, 28, "dani", "dan","NombreMadre", "Johana");
+		Usuarios.add(admon3);
+		Usuario user=new Usuario("superuser", true, 0, "user", "user", "user", "user");
+		Usuarios.add(user);
+		Usuario user2 = new Usuario("Pablo",true,19, "holasoypablo","bat123", "NombrePadre", "Jorge");
+	}
+	public static void cargarProductos() {
+		productos=new ArrayList<Producto>();
+		productos.add(new Producto("Leche","1 Litro",3500,3000));
+		productos.add(new Producto("Yogur","1.2 Litros",3500,3000));
+		productos.add(new Producto("Almendras","Enteras con cascara 250gr",8900,3000));
+		productos.add(new Producto("Frijoles","500gr",3500,3000));
+		productos.add(new Producto("Lentejas","400ml",3500,3000));
+		productos.add(new Producto("Portatil","Ryzen 7",2000000,1450000));
+		productos.add(new Producto("Celular","Mejor calida Precio del mercado",1200000,650000));
+		productos.add(new Producto("Horno","Haceb",350000,260000));
+		productos.add(new Producto("Licuadora","Oster",75000,35000));
+	}
+	public static void cargarInventario() {
+		inventario=new Inventario();
+		for (int i=0;i<productos.size();i++) {
+			inventario.AddInventario(new Detalle(productos.get(i),(int)(Math.random() * (100 - 0 + 1) + 0)));
+		}
+	}
+	public static void cargarCategorias() {
+		categorias=new ArrayList<Categoria>();
+		categorias.add(new Categoria("Lacteos","Derivados de la leche"));
+		categorias.add(new Categoria("Granos","Semillas entera"));
+		categorias.add(new Categoria("Enlatados","Empacados para durar"));
+		categorias.add(new Categoria("Tecnologia","El avance es el futuro"));
+		categorias.add(new Categoria("Electrodomesticos","Todo para el hogar"));
+	}
+	public static boolean isNumeric(String s) {
+		try {
+			Integer.parseInt(s);
+		}
+		catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void init() {
+		cargarUsuarios();
+		cargarProductos();
+		cargarCategorias();
+		cargarInventario();
+	}
+	
+}

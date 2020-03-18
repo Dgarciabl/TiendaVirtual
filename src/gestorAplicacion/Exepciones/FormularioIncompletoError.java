6b3df5bc4d7f4083d698ae628286f04e629198c0
holasoypablo <jubuitrago@unal.edu.co>
2020-03-18 15:51:22 -0500

    constructores excepciones
    a

--- src/gestorAplicacion/Exepciones/FormularioIncompletoError.java
+++ src/gestorAplicacion/Exepciones/FormularioIncompletoError.java
@@ -1,5 +1,7 @@
 package gestorAplicacion.Exepciones;
 
 public class FormularioIncompletoError extends ErrorAplicacion {
-
+	public FormularioIncompletoError() {
+		super();  //aqui debera llamar a errorAplicacion e imprimir “Manejo de errores de la Aplicación:”
+	}
 }

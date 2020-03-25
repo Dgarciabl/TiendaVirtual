f2e751fbfe6de30623b66ad745017a0612d73058
Dgarciabl <Dgarciabl@users.noreply.github.com>
2020-03-25 18:13:05 -0500

    changes fieldpane

--- src/UImain/FieldPane.java
+++ src/UImain/FieldPane.java
@@ -1,6 +1,9 @@
 package UImain;
 
 import javafx.scene.layout.Pane;
+
+import java.util.ArrayList;
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.*;
@@ -8,9 +11,13 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 public class FieldPane extends Pane {
 	GridPane child=new GridPane();
+	ArrayList<Label> titulos=new ArrayList<Label>();
+	ArrayList<TextField> vals=new ArrayList<TextField>();
 	public FieldPane(String tituloCriterios, String[] criterios, String tituloValores, String[] valores, boolean[] habilitado) {
-		child.add(new Label(tituloCriterios), 0, 0);
-		child.add(new Label(tituloValores), 1, 0);
+		titulos.add(new Label(tituloCriterios));
+		titulos.add(new Label(tituloValores));
+		child.add(titulos.get(0), 0, 0);
+		child.add(titulos.get(1), 1, 0);
 		if (habilitado==null) {
 			habilitado=new boolean[criterios.length];
 			for(int i=0;i<criterios.length;i++) {
@@ -19,23 +26,26 @@ public class FieldPane extends Pane {
 		}
 		if (valores==null) {
 			for(int i=0;i<criterios.length;i++){
-				child.add(new Label(criterios[i]), 0, i+1);
-				TextField hijo=new TextField();
-				hijo.setEditable(habilitado[i]);
-				child.add(hijo, 1, i+1);
+				titulos.add(new Label(criterios[i]));
+				child.add(titulos.get(2+i), 0, i+1);
+				vals.add(new TextField());
+				vals.get(i).setEditable(habilitado[i]);
+				child.add(vals.get(i), 1, i+1);
 			}
 		}else {
 			for(int i=0;i<criterios.length;i++){
 				if(valores[i]==null) {
-					child.add(new Label(criterios[i]), 0, i+1);
-					TextField hijo=new TextField();
-					hijo.setEditable(habilitado[i]);
-					child.add(hijo, 1, i+1);
+					titulos.add(new Label(criterios[i]));
+					child.add(titulos.get(2+i), 0, i+1);
+					vals.add(new TextField());
+					vals.get(i).setEditable(habilitado[i]);
+					child.add(vals.get(i), 1, i+1);
 				}else {
-					child.add(new Label(criterios[i]), 0, i+1);
-					TextField hijo=new TextField(valores[i]);
-					hijo.setEditable(habilitado[i]);
-					child.add(hijo, 1, i+1);
+					titulos.add(new Label(criterios[i]));
+					child.add(titulos.get(2+i), 0, i+1);
+					vals.add(new TextField(valores[i]));
+					vals.get(i).setEditable(habilitado[i]);
+					child.add(vals.get(i), 1, i+1);
 				}
 			}
 		}
@@ -49,6 +59,11 @@ public class FieldPane extends Pane {
 	*/
 	public String getValue(String criterio) {
 		return "";
+	}
+	public void setHabilitado(boolean[] habilitado) {
+		for(int i=0;i<vals.size();i++) {
+			vals.get(i).setEditable(habilitado[i]);
+		}
 	}
 	public GridPane getChild() {
 		return child;

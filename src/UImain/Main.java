73dcbb10509fb5338e00b7aea1ffed383aa415ac
JulianFM1 <JulianFM1@users.noreply.github.com>
2020-03-27 05:23:53 -0500

    Errores

--- src/UImain/Main.java
+++ src/UImain/Main.java
@@ -2081,7 +2081,7 @@ public class Main extends Application {
 		crear.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				Alert al = new Alert(AlertType.NONE);
+				Alert al = new Alert(AlertType.ERROR);
 				try {
 					String nombre = columnas.getValue(0);
 					String descripcion = columnas.getValue(1);
@@ -2189,7 +2189,7 @@ public class Main extends Application {
 		crear.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				Alert al = new Alert(AlertType.NONE);
+				Alert al = new Alert(AlertType.ERROR);
 				try {
 					String nombre = columnas.getValue(0);
 					String descripcion = columnas.getValue(1);
@@ -2227,7 +2227,7 @@ public class Main extends Application {
 					al.setTitle("Nombre duplicado");
 				}
 				al.show();
-				for(int i=0; i<3;i++) {
+				for(int i=0; i<2;i++) {
 					columnas.getBox(i).clear();
 				}
 			}
@@ -2279,7 +2279,7 @@ public class Main extends Application {
 		crear.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				Alert al = new Alert(AlertType.NONE);
+				Alert al = new Alert(AlertType.ERROR);
 				try {
 					String nombre = columnas.getValue(0);
 					int edad = Integer.valueOf(columnas.getValue(1));

7b74270ab8fb12e3eeb2ef73d13714d24837af64
holasoypablo <jubuitrago@unal.edu.co>
2020-03-27 03:56:04 -0500

    cambios

--- src/gestorAplicacion/Exepciones/NombreDuplicado.java
+++ src/gestorAplicacion/Exepciones/NombreDuplicado.java
@@ -2,7 +2,7 @@ package gestorAplicacion.Exepciones;
 
 public class NombreDuplicado extends ExcepcionUsuario {
 	public NombreDuplicado() {
-		super(" Permiso denegado");  
+		super(" Nombre duplicado!");  
 	}
 
 }

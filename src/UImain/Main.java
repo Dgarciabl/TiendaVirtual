b36ac89168658be86b3118e994f3c8855b70a935
VinNeko-ZX <55595309+VinNeko-ZX@users.noreply.github.com>
2020-03-26 20:23:19 -0500

    as

--- src/UImain/Main.java
+++ src/UImain/Main.java
@@ -11,8 +11,6 @@ import gestorAplicacion.Usuario.*;
 import gestorAplicacion.Administrador.*;
 import gestorAplicacion.Exepciones.FormularioIncompletoError;
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -31,7 +29,6 @@ import javafx.scene.input.MouseEvent;
 public class Main extends Application {
 	//App data
 	public static int hojaActual;
-	public static int indice;
 	public static ArrayList<Persona> Usuarios;
 	public static Inventario inventario;
 	public static ArrayList<Categoria> categorias;
@@ -1337,33 +1334,21 @@ public class Main extends Application {
 		
 		FieldPane columnas = new FieldPane(" ",campos, " ", empty, null);
 		columnas.getChild().getChildren().remove(columnas.getBox(4));
-
 		String[] categ = new String[categorias.size()];
 		for(int i = 0; i<categorias.size(); i++) {
 			categ[i]= categorias.get(i).getNombre();
 		}
+
 		ComboBox cats=new ComboBox(FXCollections.observableArrayList(categ));
-		cats.setPromptText("Categorias");
 		cats.setValue("Ninguno");
-		cats.valueProperty().addListener(new ChangeListener<String>() {
-			@Override
-			public void changed(ObservableValue observable, String oldValue, String newValue) {
-				for(int i=0; i<categorias.size();i++) {
-					if(categorias.get(i).getNombre().equals(newValue)) {
-						indice = i;
-					}
-				}
-			}
-			
-		});
-		
-		columnas.getChild().add(cats, 1, 5);
+		cats.setPromptText("Categorias");
+		columnas.getChild().add(cats, 2, 5);
 		GridPane botones = new GridPane();
 		Button salir = new Button("Salir");
 		salir.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				archivo();
+				finalizar();
 			}
 		});
 		Button crear = new Button("Crear");
@@ -1374,7 +1359,7 @@ public class Main extends Application {
 				String descripcion = columnas.getValue(1);
 				int oPrice = Integer.valueOf(columnas.getValue(2));
 				int sPrice = Integer.valueOf(columnas.getValue(3));
-				Producto productoCreado = new Producto(nombre,descripcion,oPrice,sPrice,categorias.get(indice));
+				Producto productoCreado = new Producto(nombre,descripcion,oPrice,sPrice);
 				productos.add(productoCreado);
 				Alert info = new Alert(AlertType.INFORMATION);
 				info.setHeaderText("Producto creado");
@@ -1415,22 +1400,7 @@ public class Main extends Application {
 		salir.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				archivo();
-			}
-		});
-		Button crear = new Button("Crear");
-		crear.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				String nombre = columnas.getValue(0);
-				String descripcion = columnas.getValue(1);
-				Categoria categoriaCreada = new Categoria(nombre,descripcion);
-				Alert info = new Alert(AlertType.INFORMATION);
-				info.setHeaderText("Categoria creado");
-				info.setTitle("Información");
-				info.show();
-				categorias.add(categoriaCreada);
-				
+				finalizar();
 			}
 		});
 		botones.add(salir, 0, 0);
@@ -1495,7 +1465,7 @@ public class Main extends Application {
 				Usuario usuariocreado = new Usuario(nombre,genero,edad,usuario,contrasena,question,answer,saldo);
 				Usuarios.add(usuariocreado);
 				Alert info = new Alert(AlertType.INFORMATION);
-				info.setHeaderText("Usuario creado");
+				info.setHeaderText("Producto creado");
 				info.setTitle("Información");
 				info.show();
 			}

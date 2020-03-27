0f36d7ccbc613e9c9f1daef39abc49661424b338
JulianFM1 <JulianFM1@users.noreply.github.com>
2020-03-27 05:02:32 -0500

    min

--- src/UImain/Main.java
+++ src/UImain/Main.java
@@ -2041,11 +2041,8 @@ public class Main extends Application {
 					al.setHeaderText("Producto creado");
 					al.setTitle("Información");
 					
-				}catch(FormularioIncompletoError e2) {
-					al.setAlertType(AlertType.ERROR);
-					al.setHeaderText(e2.getMessage());
-					al.setTitle("Formulario Incompleto");
-				}catch (NumberFormatException e1){
+				}
+				catch (NumberFormatException e1){
 					try {
 						throw new InputError();
 					}catch(InputError e4) {
